4ab8035bc8cdf09fcb3588cf80196011e13058c1
stemisruler <ralralralila@gmail.com>
2023-05-15 18:00:44 +0900

    dd

--- js/sw.js
+++ js/sw.js
@@ -1,11 +1,13 @@
 // Define the files to cache
 const CACHE_NAME = "my-web-app-cache";
 const urlsToCache = [
-  "/index.html",
-  "/js/quotes.js",
-  "/images/a_be.png",
-  "/images/a_re.png",
-  "/images/a_happy.png",
+  "index.html",
+  "js/quotes.js",
+  "css/index.css",
+  "css/header-footer.css",
+  "images/a_be.png",
+  "images/a_re.png",
+  "images/a_happy.png",
 ];
 
 // Open the cache and add the files
